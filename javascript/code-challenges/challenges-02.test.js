16314fd43c1bbdd91b68ea055f7d2edde65a437f
erik savage <erik.k.savage@gmail.com>
2021-11-30 14:00:20 -0800

    completes extractAbilities() to pass challenge 9

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -170,6 +170,8 @@ const snorlaxAbilities = {
 
 const extractAbilities = (arr) => {
   // Solution code here...
+  let abilityNames = arr.map(abilitySlot => abilitySlot.ability.name);
+  return abilityNames;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -310,7 +312,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should return an array containing only the ability names', () => {
     expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
     expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
