5dd4f97989f4ff07ed8f2523da6ec8f72da2ba27
erik savage <erik.k.savage@gmail.com>
2021-11-30 13:14:14 -0800

    completes raisedToTheThird() to pass challenge 1

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -10,6 +10,8 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 
 const raisedToTheThird = (arr) => {
   // Solution code here...
+  let newArray = arr.map(arrayNumber => Math.pow(arrayNumber, 3));
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
