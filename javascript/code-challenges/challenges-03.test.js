fbfec5607abf738bdaa080b69f2c348dbfe10d85
erik savage <erik.k.savage@gmail.com>
2021-12-01 13:38:12 -0800

    completes getStatName() to pass challenge 7

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -127,6 +127,9 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 
 const getStatName = (arr, minBaseStat) => {
   // Solution code here...
+  let greaterStat = arr.filter(arrObj => (arrObj.baseStat > minBaseStat));
+  let greaterStatName = greaterStat.map(arrObj => arrObj.stat.name);
+  return greaterStatName;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -273,7 +276,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
