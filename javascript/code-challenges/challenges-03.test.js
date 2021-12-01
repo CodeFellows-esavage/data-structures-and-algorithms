18297c82729de61e98f034e5cc424349a8c73b44
erik savage <erik.k.savage@gmail.com>
2021-12-01 13:15:52 -0800

    completes typeNum() to pass challenge 2

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -25,6 +25,8 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 
 const typeNum = (arr) => {
   // Solution code here...
+  let numArr = arr.filter(arrVal => (typeof(arrVal) === 'number'));
+  return numArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -200,7 +202,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array containing only numbers', () => {
     expect(typeNum([1, 'bob', 3])).toStrictEqual([1, 3]);
     expect(typeNum([1, 'bob', 3]).length).toStrictEqual(2);
