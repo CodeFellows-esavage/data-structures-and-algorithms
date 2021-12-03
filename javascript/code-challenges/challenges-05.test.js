89f44d9bab5d32e13630e798ab5e9a66139b06ae
erik savage <erik.k.savage@gmail.com>
2021-12-03 15:24:59 -0800

    completes countPrimeNumbers() to pass challenge 9

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -244,6 +244,13 @@ const isPrime = (value) => {
 
 const countPrimeNumbers = (arr) => {
   // Solution code here...
+  return arr.reduce((primeCount, currentNum) =>{
+    if(isPrime(currentNum)){
+      return primeCount + 1;
+    } else {
+      return primeCount;
+    }
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -369,7 +376,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should return a count of the prime numbers in the array', () => {
     expect(countPrimeNumbers([1, 2, 13, 64, 45, 56, 17, 8])).toStrictEqual(3);
   });
