fde421eb42a55c809b0faa8a142f3076d692d519
erik savage <erik.k.savage@gmail.com>
2021-12-03 14:11:24 -0800

    completes addValues() to pass challenge 2

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -25,8 +25,11 @@ Write a function named addValues that, given an array of numbers as input, uses 
 
 const addValues = (arr) => {
   // Solution code here...
+  return arr.reduce((pV, cV) => {
+    console.log(pV, cV);
+    return pV + cV; //sets pV
+  }, 0);
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
@@ -290,7 +293,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should add the values of an array', () => {
     expect(addValues([1, 2, 3, 4, 5])).toStrictEqual(15);
     expect(addValues([])).toStrictEqual(0);
