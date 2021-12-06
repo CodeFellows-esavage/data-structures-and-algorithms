237651d0da9cf2a1ac0a7223f208cc21e9a29604
erik savage <erik.k.savage@gmail.com>
2021-12-06 13:31:48 -0800

    completes appendTheEnd() to pass challenge 2

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -41,6 +41,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  let newStr = `${str} The end.`;
+  return newStr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -134,7 +136,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should append without modifying the oiginal', () => {
     const a = 'This is my story.';
     const b = appendTheEnd(a);
