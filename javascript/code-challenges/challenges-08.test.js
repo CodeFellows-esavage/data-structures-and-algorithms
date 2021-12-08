d819762c0a2d2b7da4539bcd53a3c145e070c99e
erik savage <erik.k.savage@gmail.com>
2021-12-08 13:37:14 -0800

    completes containsWorld() to pass challenge 4

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -112,6 +112,8 @@ Write a function named containsWorld that takes in a string or number of any len
 
 const containsWorld = (input) => {
   // Solution code here...
+  const regEx = /(?:world)/g;
+  return regEx.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -242,7 +244,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return true if the input contains the word school in lower case', () => {
     expect(containsWorld('hello world')).toBe(true);
   });
