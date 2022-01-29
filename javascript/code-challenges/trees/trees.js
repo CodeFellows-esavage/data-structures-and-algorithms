131343509e5d28d273b9290cb0a53e9cb618adcf
erik savage <erik.k.savage@gmail.com>
2022-01-29 15:30:49 -0800

    adds methods preOrder, inOrder, postOrder, and add (BST only)

--- javascript/code-challenges/trees/trees.js
+++ javascript/code-challenges/trees/trees.js
@@ -15,16 +15,55 @@ class BinaryTree {
 
   preOrder() {
     //READ: current value -> left value -> right value
+    const order = [];
+    let current = this.root;
 
+    if (!current) return 'Tree is empty';
 
+    let traverse = (node) => {
+      order.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(current);
+    return order;
 
   }
+
   inOrder() {
     //READ: left value -> current value -> right value
+    const order = [];
+    let current = this.root;
+
+    if (!current) return 'Tree is empty';
+
+    let traverse = (node) => {
+      if (node.left) traverse(node.left);
+      order.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+
+    traverse(current);
+    return order;
 
   }
+
   postOrder() {
     //READ: left value -> right value -> current value
+    const order = [];
+    let current = this.root;
+
+    if (!current) return 'Tree is empty';
+
+    let traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      order.push(node.value);
+    };
+
+    traverse(current);
+    return order;
 
   }
 }
@@ -33,8 +72,38 @@ class BST extends BinaryTree {
   // constructor(){
 
   // }
-  add() {
+  add(value) {
+    //allow for duplicates on the right
+    if (typeof (value) !== 'number') return 'Value must be a number';
 
+    let node = new Node(value);
+
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      let traverse = true;
+      while (traverse) {
+        if (node.value < current.value && current.left) {
+          current = current.left;
+        } else if (node.value < current.value) {
+          //put our new node here
+          current.left = node;
+          traverse = false;
+        }
+        if (node.value >= current.value && current.right) {
+          current = current.right;
+        } else if (node.value >= current.value) {
+          //put our new node here
+          current.right = node;
+          traverse = false;
+        }
+      }
+    }
+  }
+
+  contains(value) {
+    //return true or false if the value is in the tree
   }
 }
 
@@ -56,3 +125,21 @@ n3.left = n6;
 n3.right = n7;
 
 console.log(JSON.stringify(tree));
+console.log(tree.preOrder());
+console.log(tree.inOrder());
+console.log(tree.postOrder());
+
+let bsTree = new BST;
+
+console.log(bsTree);
+bsTree.add(10);
+bsTree.add(8);
+bsTree.add(14);
+bsTree.add(5);
+bsTree.add(9);
+bsTree.add(12);
+bsTree.add(17);
+console.log(JSON.stringify(bsTree));
+
+
+
