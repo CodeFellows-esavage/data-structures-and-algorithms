a502d6cde3a2107b44a2debfa06bfffbb25f16a3
erik savage <erik.k.savage@gmail.com>
2022-02-02 16:21:12 -0800

    fixed bug creating multiple new nodes, exports modules

--- javascript/code-challenges/tree-fizz-buzz/tree-fizz-buzz.js
+++ javascript/code-challenges/tree-fizz-buzz/tree-fizz-buzz.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const treeify = require('treeify');
+// const treeify = require('treeify');
 
 class Node {
   constructor(value) {
@@ -24,13 +24,13 @@ function fizzBuzzTree(tree) {
   let traverse = (node) => {
     let value;
     if (node.value % 3 === 0 && node.value % 5 === 0) {
-      value = new Node('FizzBuzz');
+      value = 'FizzBuzz';
     } else if (node.value % 3 === 0) {
-      value = new Node('Fizz');
+      value = 'Fizz';
     } else if (node.value % 5 === 0) {
-      value = new Node('Buzz');
+      value = 'Buzz';
     } else {
-      value = new Node(`${node.value}`);
+      value = `${node.value}`;
     }
 
     const newNode = new Node(value);
@@ -49,30 +49,34 @@ function fizzBuzzTree(tree) {
   return newTree;
 }
 
-let startingTree = new KaryTree(3);
+// let startingTree = new KaryTree(3);
 
-let n1 = new Node(10);
-let n2 = new Node(3);
-let n3 = new Node(5);
-let n4 = new Node(8);
-let n5 = new Node(15);
-let n6 = new Node(7);
-let n7 = new Node(25);
-let n8 = new Node(14);
-let n9 = new Node(9);
-let n10 = new Node(18);
-let n11 = new Node(30);
-let n12 = new Node(1);
+// let n1 = new Node(10);
+// let n2 = new Node(3);
+// let n3 = new Node(5);
+// let n4 = new Node(8);
+// let n5 = new Node(15);
+// let n6 = new Node(7);
+// let n7 = new Node(25);
+// let n8 = new Node(14);
+// let n9 = new Node(9);
+// let n10 = new Node(18);
+// let n11 = new Node(30);
+// let n12 = new Node(1);
 
-n1.children = [n2, n3];
-n2.children = [n4, n5, n6];
-n3.children = [n8, n9];
-n5.children = [n7];
-n8.children = [n10, n11, n12];
+// n1.children = [n2, n3];
+// n2.children = [n4, n5, n6];
+// n3.children = [n8, n9];
+// n5.children = [n7];
+// n8.children = [n10, n11, n12];
 
-startingTree.root = n1;
-console.log(treeify.asTree(startingTree, true));
-let newTree = fizzBuzzTree(startingTree);
-console.log(treeify.asTree(newTree, true));
+// startingTree.root = n1;
+// console.log(treeify.asTree(startingTree, true));
+// let newTree = fizzBuzzTree(startingTree);
+// console.log(treeify.asTree(newTree, true));
 
-
+module.exports = {
+  Node,
+  KaryTree,
+  fizzBuzzTree,
+};
