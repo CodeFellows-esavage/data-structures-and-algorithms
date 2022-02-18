b70f8da3509ed01b52d5695b1c7e81d5f2ac8300
erik savage <erik.k.savage@gmail.com>
2022-02-18 10:37:04 -0800

    file initialization

---
+++ javascript/code-challenges/hash-table/__tests__/hash-table.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Testing Hashtable Class', () => {
+  xit('should store a key/value in the data structure', () => {
+
+  });
+  xit('should retreive a value based on the key', () => {
+
+  });
+  xit('should return null for a key that does not exist', () => {
+
+  });
+  xit('should beable to return a list of all unique keys in the table', () => {
+
+  });
+  xit('should succesfully handle a collision', () => {
+
+  });
+  xit('should successfully retrieve a value from a bucket within a table that has a collision', () => {
+
+  });
+  xit('should successfully hash a key to an in-range value', () => {
+
+  });
+});
